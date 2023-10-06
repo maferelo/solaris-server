5807dea07aa8f1efaaa565b007981ae2b5a21c4c
Felipe Restrepo <maferelo@outlook.com>
2023-10-06 19:36:16 +0000

    chore(precommit): black formatting on manage.py

--- manage.py
+++ manage.py
@@ -4,9 +4,10 @@ import os
 import sys
 from pathlib import Path
 
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,14 +16,14 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-        
+
     # This allows easy placement of apps within the interior
     # solaris directory.
     current_path = Path(__file__).parent.resolve()
     sys.path.append(str(current_path / "apps"))
-    
+
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
