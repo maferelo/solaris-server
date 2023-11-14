fbe5e3566367790624104576545f3f3047667df7
Felipe Restrepo <maferelo@users.noreply.github.com>
2023-11-14 10:50:58 -0500

    feat(users): enable groups (#9)

--- apps/trips/tests/test_http.py
+++ apps/trips/tests/test_http.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
@@ -7,35 +8,33 @@ from trips.models import Trip
 PASSWORD = "pAssw0rd!"
 
 
-def create_user(phone="+3017839876", password=PASSWORD):
-    return get_user_model().objects.create_user(phone=phone, password=password)
+def create_user(phone="+3017839876", password=PASSWORD, group_name="rider"):
+    group, _ = Group.objects.get_or_create(name=group_name)
+    user = get_user_model().objects.create_user(phone=phone, password=password)
+    user.groups.add(group)
+    user.save()
+    return user
 
 
 class HttpTripTest(APITestCase):
     def setUp(self):
-        user = create_user()
-        response = self.client.post(
-            reverse("users:log_in"),
-            data={
-                "phone": user.phone,
-                "password": PASSWORD,
-            },
-        )
-        self.access = response.data["access"]
+        self.user = create_user()
+        self.client.login(username=self.user.phone, password=PASSWORD)
 
     def test_user_can_list_trips(self):
         trips = [
-            Trip.objects.create(pick_up_address="A", drop_off_address="B"),
-            Trip.objects.create(pick_up_address="B", drop_off_address="C"),
+            Trip.objects.create(pick_up_address="A", drop_off_address="B", rider=self.user),
+            Trip.objects.create(pick_up_address="B", drop_off_address="C", rider=self.user),
+            Trip.objects.create(pick_up_address="C", drop_off_address="D"),
         ]
-        response = self.client.get(reverse("trips:trip_list"), headers={"authorization": f"Bearer {self.access}"})
+        response = self.client.get(reverse("trips:trip_list"))
         self.assertEqual(status.HTTP_200_OK, response.status_code)
-        exp_trip_ids = [str(trip.id) for trip in trips]
+        exp_trip_ids = [str(trip.id) for trip in trips[0:2]]
         act_trip_ids = [trip.get("id") for trip in response.data]
-        self.assertCountEqual(exp_trip_ids, act_trip_ids)
+        self.assertCountEqual(act_trip_ids, exp_trip_ids)
 
     def test_user_can_retrieve_trip_by_id(self):
-        trip = Trip.objects.create(pick_up_address="A", drop_off_address="B")
-        response = self.client.get(trip.get_absolute_url(), headers={"authorization": f"Bearer {self.access}"})
+        trip = Trip.objects.create(pick_up_address="A", drop_off_address="B", rider=self.user)
+        response = self.client.get(trip.get_absolute_url())
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(str(trip.id), response.data.get("id"))
