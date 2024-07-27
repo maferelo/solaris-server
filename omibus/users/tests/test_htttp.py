17c374e508462dc30b7331735ea02391cc9809ea
Felipe Restrepo <maferelo@outlook.com>
2024-07-27 16:19:54 +0000

    chore: update TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, and TWILIO_SERVICE_SID environment variables

--- omibus/users/tests/test_htttp.py
+++ omibus/users/tests/test_htttp.py
@@ -30,7 +30,6 @@ def create_user(phone="+573017839876", password=PASSWORD, group_name="rider"):
 
 class AuthenticationTest(APITestCase):
     @patch("users.auth_backend.check_code")
-    @patch("users.twilio_client.Client")
     def test_user_can_log_in(self, mock_check_code):
         user = create_user()
         mock_check_code.return_value = True
