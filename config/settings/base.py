113640cfecf6baf145a1c6edc1782402bcf239a1
Felipe Restrepo <maferelo@outlook.com>
2024-07-27 19:03:34 +0000

    feat: geodjango deps and setup with postgis

--- config/settings/base.py
+++ config/settings/base.py
@@ -45,6 +45,7 @@ USE_TZ = True
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {"default": env.db("DATABASE_URL")}
+DATABASES["default"]["ENGINE"] = ("django.contrib.gis.db.backends.postgis",)
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
@@ -67,6 +68,7 @@ DJANGO_APPS = [
     "django.contrib.postgres",
     "django.contrib.staticfiles",
     "django.contrib.admin",
+    "django.contrib.gis",
 ]
 
 THIRD_PARTY_APPS = [
