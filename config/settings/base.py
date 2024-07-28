7fecbb87a90f3d2e5af673e08e7e1d3b60375098
Felipe Restrepo <maferelo@users.noreply.github.com>
2024-07-27 18:30:18 -0700

    Chore/disable translations (#19)
    * chore(translations): disable setting
    
    * chore: Update Celery and Twilio configuration
    
    This commit updates the Celery and Twilio configuration in the project. It sets `CELERY_TASK_ALWAYS_EAGER` to `False` and `CELERY_TASK_EAGER_PROPAGATES` to `True`. Additionally, it updates the Twilio account SID, auth token, and service SID environment variables.
    
    Recent user commits:
    - feat: geodjango deps and setup with postgis
    - chore: update TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, and TWILIO_SERVICE_SID environment variables
    - chore: update TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, and TWILIO_SERVICE_SID environment variables
    - test_user_can_log_in: mock twilio_client.Client in AuthenticationTest
    - chore: fix tests to path login check code methods
    - fix: test to use relative imports
    - feat: update CircleCI configuration to use the latest Ubuntu image
    - lint
    - lint
    - feat: celery redis and twilio

--- config/settings/base.py
+++ config/settings/base.py
@@ -35,7 +35,7 @@ LANGUAGE_CODE = "en-us"
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
 SITE_ID = 1
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
-USE_I18N = True
+USE_I18N = False
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-tz
 USE_TZ = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#locale-paths
@@ -278,6 +278,29 @@ SPECTACULAR_SETTINGS = {
     "VERSION": "1.0.0",
     "SERVE_PERMISSIONS": ["rest_framework.permissions.IsAdminUser"],
 }
+
+# Channels
+# ------------------------------------------------------------------------------
+REDIS_URL = env("REDIS_URL")
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [REDIS_URL],
+        },
+    },
+}
+
+# Celery
+# ------------------------------------------------------------------------------
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-always-eager
+CELERY_TASK_ALWAYS_EAGER = False
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-eager-propagates
+CELERY_TASK_EAGER_PROPAGATES = True
+
+# Twilio
+# ------------------------------------------------------------------------------
 
 TWILIO_ACCOUNT_SID = env("TWILIO_ACCOUNT_SID", default="dummy")
 TWILIO_AUTH_TOKEN = env("TWILIO_AUTH_TOKEN", default="dummy")
--- config/settings/local.py
+++ config/settings/local.py
@@ -52,23 +52,3 @@ INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
-
-# Channels
-# ------------------------------------------------------------------------------
-REDIS_URL = env("REDIS_URL")
-
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [REDIS_URL],
-        },
-    },
-}
-
-# Celery
-# ------------------------------------------------------------------------------
-# https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-always-eager
-CELERY_TASK_ALWAYS_EAGER = False
-# https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-eager-propagates
-CELERY_TASK_EAGER_PROPAGATES = True
