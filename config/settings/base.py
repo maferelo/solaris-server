86bd3fe20bf5ee6666409c2046562ace3bf103bc
Felipe Restrepo <maferelo@outlook.com>
2024-07-28 02:18:32 +0000

    chore(settings): databases engine

--- config/settings/base.py
+++ config/settings/base.py
@@ -45,7 +45,7 @@ USE_TZ = True
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {"default": env.db("DATABASE_URL")}
-DATABASES["default"]["ENGINE"] = ("django.contrib.gis.db.backends.postgis",)
+DATABASES["default"]["ENGINE"] = "django.contrib.gis.db.backends.postgis"
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
